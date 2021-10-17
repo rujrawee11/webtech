c7a09c5a5166af0a6fefc5bc4ef9c2cb561a8177
Aphatsara Moratsathian <63070188@kmitl.ac.th>
2021-10-17 17:46:07 +0700

    javascript

---
+++ script.js
@@ -0,0 +1,17 @@
+let parallaxImage = document.getElementById('ParallaxImage');
+let parallaxContent = document.getElementById('ParallaxContent');
+let topbar = document.getElementById('topbar');
+let windowScrolled;
+
+window.addEventListener('scroll', function windowScroll() {
+    windowScrolled = window.pageYOffset || this.document.documentElement.scrollTop;
+    console.log(windowScrolled);
+    if (windowScrolled >= 250){
+        topbar.style.visibility = 'hidden';
+    }else{
+        topbar.style.visibility = 'visible';
+        topbar.style.backgroundColor = "#254dcf";
+    }
+    parallaxImage.style.transform = `translate3d(0, ${windowScrolled / 2}px, 0)`;
+    parallaxContent.style.transform = `translate3d(0, ${windowScrolled / 12}px, 0)`;
+})
