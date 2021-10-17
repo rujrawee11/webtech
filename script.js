3dee022fffca26cbffda12bd9e3802c9e51527b5
Aphatsara Moratsathian <63070188@kmitl.ac.th>
2021-10-18 02:13:35 +0700

    java

--- script.js
+++ script.js
@@ -6,12 +6,12 @@ let windowScrolled;
 window.addEventListener('scroll', function windowScroll() {
     windowScrolled = window.pageYOffset || this.document.documentElement.scrollTop;
     console.log(windowScrolled);
-    if (windowScrolled >= 250){
+    if (windowScrolled >= 300){
         topbar.style.visibility = 'hidden';
     }else{
         topbar.style.visibility = 'visible';
         topbar.style.backgroundColor = "#254dcf";
     }
     parallaxImage.style.transform = `translate3d(0, ${windowScrolled / 2}px, 0)`;
-    parallaxContent.style.transform = `translate3d(0, ${windowScrolled / 12}px, 0)`;
+    parallaxContent.style.transform = `translate3d(0, ${windowScrolled / 10}px, 0)`;
 })
