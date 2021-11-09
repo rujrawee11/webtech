063506da3d283f447bce49f0c483a7855b50535d
Booeeiii <73687103+Booeeiii@users.noreply.github.com>
2021-11-09 17:04:06 +0700

    Add files via upload

---
+++ add.php
@@ -0,0 +1,36 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo "<div class='card mt-5' style='height: 27vh;'>
+    <div class='row col-12'>
+        <div class='col-4 h-80'
+            style='border-top-left-radius: 20px; border-bottom-left-radius: 20px; background-image: url(https://static.wixstatic.com/media/58d396_9852b43d65404c4ca5749c01676ca5d8~mv2.jpeg/v1/fill/w_295,h_188,al_c,q_80,usm_0.66_1.00_0.01/58d396_9852b43d65404c4ca5749c01676ca5d8~mv2.webp); background-repeat: no-repeat; background-size: 100%; height: 100%;'>
+        </div>
+        <div class='card-body h-10 col-8'>
+            <h1 class='card-title' id='cardtitle'><b>
+                พิสูจน์อักษรหนังสือที่จะใช้พิมพ์อักษรเบลล์</b></h1>
+
+            <p class='card-text' id='cardcontent'>พิสูจน์เอกสารทางมือถือ หรือคอมพ์ ฯ เพื่อส่งมอบให้หน่วยงานที่เกี่ยวข้อง ไปทำเป็นหนังสืออักษรเบลล์
+            </p>
+            <br>
+            <div class='h-10 d-flex align-items-center justify-content-end'>
+                <a href='#' class='text-secondary' id='inform'>ดูรายละเอียด <svg
+                        xmlns='http://www.w3.org/2000/svg' width='16' height='14'
+                        fill='currentColor' class='bi bi-arrow-right' viewBox='0 0 16 16'>
+                        <path fill-rule='evenodd'
+                            d='M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z' />
+                    </svg></a>
+            </div>
+        </div>
+    </div>
+</div>"
+    ?>
+    
+</body>
+</html>
---
+++ suggest.js
@@ -0,0 +1,17 @@
+
+let parallaxContent = document.getElementById('ParallaxContent');
+let topbar = document.getElementById('topbar');
+let windowScrolled;
+
+window.addEventListener('scroll', function windowScroll() {
+    windowScrolled = window.pageYOffset || this.document.documentElement.scrollTop;
+    console.log(windowScrolled);
+    if (windowScrolled >= 300){
+        topbar.style.visibility = 'hidden';
+    }else{
+        topbar.style.visibility = 'visible';
+        topbar.style.backgroundColor = "#254dcf";
+    }
+
+    parallaxContent.style.transform = `translate3d(0, ${windowScrolled / 10}px, 0)`;
+})
